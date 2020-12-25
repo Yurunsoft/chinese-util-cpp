e355025ea886cb04a4327e0e66767c0ba8fbe6c6
Yurun <admin@yurunsoft.com>
2020-12-25 14:51:13 +0800

    修复

--- .github/workflows/bin/install-php7.4.sh
+++ .github/workflows/bin/install-php7.4.sh
@@ -4,7 +4,7 @@ add-apt-repository ppa:ondrej/php -y -u
 
 apt-get update
 
-apt install -y php7.4-cli php7.4-bcmath php7.4-curl php7.4-dev hp7.4-mbstring php7.4-zip
+apt install -y gcc php7.4-cli php7.4-bcmath php7.4-curl php7.4-dev hp7.4-mbstring php7.4-zip
 
 php -v
 php -m
